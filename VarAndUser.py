06f07cc6fc6caddfb44079d1f3fafeb9c50078f5
Daniel Hu <d4niel.hu14@gmail.com>
2021-02-21 21:10:25 +0000

    Finished Variables and Input, fixed .replit

---
+++ VarAndUser.py
@@ -0,0 +1,12 @@
+# Daniel Hu - Variables and User Input - 2/12/2021
+
+#prompting user for name
+name = input("Please enter your name: ")
+print("Welcome " + name + ".")
+
+#prompting user for age
+age = int(input("How are old you: "))
+age = age + 10
+
+print(name + ", you will be " + str(age) + " years old in 10 years.")
+
